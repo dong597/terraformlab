899804dfe117e1b1437f2a50151a5be1d3bddff0
terraform user <kdk082687@gmail.com>
2024-10-13 04:13:03 +0900

    use busybox httpd complete

--- terraformdata.tf
+++ terraformdata.tf
@@ -2,7 +2,7 @@
 #  connection {
 #    type = "ssh"
 #    user = "ec2-user"
-#    private_key = "${file("/root/terraformlab/lab5_aws/awskey.pem")}"
+#    private_key = file("/root/terraformlab/lab5_aws/awskey.pem")
 #    host =  aws_instance.instance.public_ip 
 #  }
 #
@@ -13,25 +13,14 @@
 #
 #  provisioner "remote-exec" {
 #    inline = [
-#      "yum groupinstall 'Development Tools' -y",
-#      "sudo wget -P /root https://busybox.net/downloads/busybox-1.27.2.tar.bz2",  # 루트 디렉토리에 다운로드
-#      "sudo tar -xjf /root/busybox-1.27.2.tar.bz2 -C /root",  # 루트 디렉토리에서 압축 해제
-#      "cd /root/busybox-1.27.2",
-#      "make defconfig",
-#      "make",
-#      "make install",
-#      "chmod u+s /root/busybox-1.27.2/_install/bin/busybox",  # setuid 설정
-#      "export PATH=\\$PATH:/root/busybox-1.27.2/_install/bin",
-#      "mkdir -p /var/www",
-#      "echo 'hello world' | tee /var/www/index.html",
-#      "/root/busybox-1.27.2/_install/bin/busybox httpd -f -p 8080 &"  # 경로를 명시적으로 지정
-#    ]
+#      "sudo yum -y install httpd" ,
+#      "echo 'Eden Hazard Blues legend' | sudo tee /var/www/html/index.html",
+#      "sudo mv /tmp/httpd.conf /etc/httpd/conf/httpd.conf",
+#      "sudo systemctl start httpd" 
+#      ]
 #  }
 #
-#
-#  triggers_replace = [
+#  triggers_replace = [ 
 #    aws_instance.instance.public_ip 
 #  ]
 #}
-#  
-#
